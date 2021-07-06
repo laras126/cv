e286228b03b02cfde64ebd266e7b39bcfedd3510
laras126 <laras126@gmail.com>
2021-07-06 19:34:20 -0400

    chore: data ordering

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ rm -r docs
 mkdir docs
 cp -r ./build/html/assets ./docs/assets 
 cp ./build/html/project/templates/cv/index.html ./docs/index.html
-echo 'Now find and replace assets with cv/assets in docs/index.html and the doc title'
+echo 'Now find and replace assets with cv/assets in docs/index.html'
