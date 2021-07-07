b13a769ed4d225eafdeb5e7f0b48c0d5cb4309cc
laras126 <laras126@gmail.com>
2021-07-07 11:36:49 -0400

    (auto) chore: build cv

--- build-cv.sh
+++ build-cv.sh
@@ -7,3 +7,5 @@ cp -r build/html/assets docs/assets
 cp build/html/project/templates/cv/index.html docs/index.html
 sed -i .bak 's/assets/cv\/assets/g' docs/index.html
 rm docs/index.html.bak
+git commit -am '(auto) chore: build cv'
+git push origin main
