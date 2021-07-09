4b112fdddbe8e43068467011c6558d4cb19838cc
laras126 <laras126@gmail.com>
2021-07-09 12:17:10 -0400

    fix: update variant

--- src/patterns/templates/cv/cv.job.js
+++ src/patterns/templates/cv/cv.job.js
@@ -1,4 +1,9 @@
-const cv = require( './cv.prototype' );
+const clonedeep = require( 'lodash.clonedeep' );
+
+const cv_prototype = require( './cv.prototype' );
+const cv = clonedeep( cv_prototype );
+
+cv.variant = 'job';
 
 cv.data.basics.label = 'Research Programmer';
 cv.data.basics.summary = 'Industry leader in web development seeking research assistant position during career transition from software engineering to computing education research.';
--- src/patterns/templates/cv/cv.prototype.js
+++ src/patterns/templates/cv/cv.prototype.js
@@ -5,6 +5,7 @@ const data = require( './data.json');
 const { mtime } = statSync( path.join( process.cwd(), 'docs/index.html') );
 
 module.exports = {
+	variant: 'prototype',
 	data,
 	mtime,
 	crop_class : 'lrv-a-crop-3x4'
