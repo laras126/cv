8f913a4fd4d3822712f76cb94d1425ca6f0d43d7
laras126 <lara@notlaura.com>
2025-10-29 21:42:54 -0400

    add delta

---
+++ src/patterns/templates/cv/cv.delta.js
@@ -0,0 +1,40 @@
+const clonedeep = require( 'lodash.clonedeep' );
+
+const cv_prototype = require( './cv.prototype-orig' );
+const cv = clonedeep( cv_prototype );
+
+cv.variant = 'academic';
+
+cv.hide_hobbies = true;
+cv.hide_service = false;
+cv.hide_profiles = true;
+
+cv.data.basics.picture = false;
+cv.data.basics.email = 'lschenck3@gatech.edu';
+cv.data.basics.label = '';
+cv.data.basics.summary = '';
+
+cv.data.basics.interests = [];
+cv.crop_class = 'lrv-a-crop-1x1';
+
+// Hide undergrad details
+cv.data.education[0].summary = "<ul><li>Advisor: Dr. Betsy DiSalvo</li></li><li>Keywords: Human-Centered AI, Future of Work, Workforce Transition, Reskilling/Upskilling, AI Product Design \& Adoption</li></ul>";
+cv.data.education[1].summary = '';
+
+cv.hide_service = true;
+
+cv.data.publications_title = "Selected Publications";
+cv.data.publications.posters = [];
+
+// Remove SNG and WhatsUp
+new_volunteer = [ cv.data.volunteer[1], cv.data.volunteer[2] ];
+cv.data.volunteer = new_volunteer;
+// cv.data.volunteer.remove(cv.data.volunteer[0]);
+// cv.data.volunteer.remove(cv.data.volunteer[2]);
+
+
+cv.data.work_title = 'Industry Experience';
+cv.data.speaking_title = 'Industry Conference Presentations';
+cv.data.outreach_title = 'Industry Outreach';
+
+module.exports = cv;
