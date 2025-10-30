8091cad4867a6aef0cfd83e302f78eae14668590
laras126 <lara@notlaura.com>
2025-10-30 18:42:24 -0400

    initial delta resume

---
+++ src/patterns/templates/cv-industry/cv-industry.delta.js
@@ -0,0 +1,27 @@
+const clonedeep = require( 'lodash.clonedeep' );
+
+const cv_prototype = require( './cv-industry.prototype' );
+const cv = clonedeep( cv_prototype );
+
+cv.data = require( './industry.json');
+
+cv.variant = 'delta';
+
+cv.hide_hobbies = true;
+cv.hide_service = false;
+cv.hide_profiles = true;
+
+cv.data.basics.picture = false;
+cv.data.basics.email = 'lschenck3@gatech.edu';
+cv.data.basics.label = 'Graduate Research Intern';
+// cv.data.basics.summary = '';
+
+cv.data.basics.interests = [];
+
+cv.data.work_title = "Work Experience";
+cv.data.volunteer_title = "Thought Leadership & Outreach";
+
+cv.data.publications_title = "Selected Publications";
+cv.data.publications.posters = [];
+
+module.exports = cv;
