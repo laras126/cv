1e9bd774a72aff1ce0e4dd0349a072fd90b10d52
laras126 <lara@notlaura.com>
2025-11-04 10:54:44 -0500

    add porsche

---
+++ src/patterns/templates/cv-industry/cv-industry.porsche.js
@@ -0,0 +1,26 @@
+const clonedeep = require( 'lodash.clonedeep' );
+
+const cv_prototype = require( './cv-industry.prototype' );
+const cv = clonedeep( cv_prototype );
+
+cv.data = require( './porsche.json');
+
+cv.variant = 'delta';
+
+cv.hide_hobbies = true;
+cv.hide_service = false;
+cv.hide_profiles = true;
+
+cv.data.basics.picture = false;
+cv.data.basics.email = 'lschenck3@gatech.edu';
+// cv.data.basics.summary = '';
+
+cv.data.basics.interests = [];
+
+cv.data.work_title = "Work Experience";
+cv.data.volunteer_title = "Professional Activities";
+
+cv.data.publications_title = "Selected Publications";
+cv.data.publications.posters = [];
+
+module.exports = cv;
--- src/patterns/templates/cv-industry/cv-industry.prototype.js
+++ src/patterns/templates/cv-industry/cv-industry.prototype.js
@@ -4,6 +4,22 @@ const path = require('path');
 const data = require( './industry.json');
 const { mtime } = statSync( path.join( process.cwd(), 'docs/index.html') );
 
+hide_hobbies = true;
+hide_service = false;
+hide_profiles = true;
+
+data.basics.picture = false;
+data.basics.email = 'lschenck3@gatech.edu';
+// data.basics.summary = '';
+
+data.basics.interests = [];
+
+data.work_title = "Work Experience";
+data.volunteer_title = "Professional Activities";
+
+data.publications_title = "Selected Publications";
+data.publications.posters = [];
+
 module.exports = {
 	variant: 'prototype',
 	data,
